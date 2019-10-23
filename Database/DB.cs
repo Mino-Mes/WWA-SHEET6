bde67eb5b08b9e025e542e6b822b96928b8308b7
Maestro-Alad <43155016+Maestro-Alad@users.noreply.github.com>
2019-10-23 15:18:42 -0400

    Add project files.

---
+++ Database/DB.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+   public class DB
+    {
+        private static lab6Entities myDb = new lab6Entities();
+        public static IEnumerable<Product> GetProducts()
+        {
+            return myDb.Products.ToList();
+        }
+        public static IEnumerable<Customer> GetCustomers()
+        {
+            return myDb.Customers.ToList();
+        }
+
+        public static IEnumerable<Sale> getSaleList()
+        {
+            return myDb.Sales.ToList();
+        }
+
+        public static Product getProduct(int id)
+        {
+            return myDb.Products.Find(id);
+        }
+        public static String UpdateCustomer(int id)
+        {
+            String fName = "Amin";
+            String lName = "Meslioui";
+            Customer cus = myDb.Customers.Find(id);
+            cus.CustomerFirstName = fName;
+            cus.CustomerLastName = lName;
+            return ("Customer: " + id + " has been updated" + " First name: " + cus.CustomerFirstName + " Last name:" + cus.CustomerLastName);
+
+        }
+        //I am not using a delete method because we should not delete but purge (save) old data.
+    }
+}
---
+++ WWA-LAB6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Database;
+
+namespace WWA_LAB6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myProdcut = DB.getProduct(1);
+
+            Console.WriteLine(myProdcut.ProductUpc.ToString());
+
+            Console.Read();
+
+            IEnumerable<Sale> sales = DB.getSaleList();
+            foreach (Sale s in sales)
+            {
+                Console.WriteLine("Sales ID:  " + s.SaleId + " \n Customer Name: " +
+                    s.Customer.CustomerId + "\n Product Description: " + s.Product.ProductDescription);
+            }
+
+            String newCustomer = DB.UpdateCustomer(1);
+            Console.WriteLine(newCustomer);
+
+        }
+    }
+}
